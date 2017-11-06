ecfc1971b815c1fd12d73156d339a811d9ca879d
Out of Ink Software <moshekrum@gmail.com>
2017-11-06 17:00:36 +0200

    tap anywhere now always works

--- views/js/taptosearch.js
+++ views/js/taptosearch.js
@@ -1,4 +1,5 @@
-document.getElementById("main").addEventListener('click', function(e){
+document.getElementById("main").addEventListener('click', function(e) {
+    if (e.target.tagName !== "H3" && e.target.tagName !== "MAIN") return    
     var query = document.getElementById("search")
     query.focus()
 });
