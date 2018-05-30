58f47e1c1bd1700a0e5a19f529e1fdc1f900af6d
Out of Ink Software <moshekrum@gmail.com>
2018-05-30 14:29:37 +0300

    now a PWA
    with new icons!

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.use(bodyParser.json())
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.locals.basedir = path.join(__dirname, 'public');
+
 app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/', function(req, res) {
