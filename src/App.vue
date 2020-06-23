36e419e144d9a7bb3bb3cbd7be3fc6bd8af8f27b
outofink <moshekrum@gmail.com>
2020-06-23 22:36:49 +0300

    basic funtionality

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,98 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AddTodo v-on:add-todo="AddTodo"/>
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+import axios from 'axios'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos, AddTodo
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          word: "help",
+          pos: "verb",
+          translation: ["עזרה", "לעזור"]
+        },
+        {
+          id: 2,
+          word: "death",
+          pos: "noun",
+          translation: ["מוות", "למות"]
+        },
+        {
+          id: 3,
+          word: "hello!",
+          pos: "exclamation",
+          translation: ["שלום!", "היי!"]
+        }
+      ]
+    }
+  },
+  methods: {
+    AddTodo(newTodo) {
+      console.log(newTodo)
+      axios.post('https://cors-anywhere.herokuapp.com/http://services.morfix.com/translationhebrew/TranslationService/GetTranslation/', 
+      {
+        Query: newTodo,
+        ClientName:"Android_Hebrew"
+      })
+      .then(res => {
+        let output = res.data
+        let json = [];
+        for (let word of output.Words) {
+          let translation = [];
+          for (let definitions of word.OutputLanguageMeanings) {
+            let buffer = [];
+            for (let definition of definitions) {
+              buffer.push(definition.DisplayText);
+            }
+            translation.push(buffer.join(', '));
+          }
+          console.log(word.ID)
+          json.push({
+            "id": word.ID,
+            'word': word.InputLanguageMeanings[0][0].DisplayText,
+            'pos': word.PartOfSpeech,
+            'translation': translation,
+          })
+        }
+        this.todos = json
+      })
+      .catch(err => console.log(err))
+    } 
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="todo-item">
+        <h1>{{todo.word}}</h1>
+        <h6>{{todo.pos}}</h6>
+        <div v-for="trans in todo.translation" :key="trans">
+            <h2>{{trans}}</h2>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"]
+}
+</script>
+
+<style scoped>
+  .todo-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+  }
+  .is-complete {
+    text-decoration: line-through;
+  }
+  .del {
+    background: #ff0000;
+    color: #fff;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+  }
+</style>
