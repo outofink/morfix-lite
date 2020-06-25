060cd228c29794bf5d74fa3a5f16b092d3de5605
outofink <moshekrum@gmail.com>
2020-06-26 02:37:44 +0300

    made text bigger

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,9 +6,9 @@
                        type="article"
     /> -->
     <v-card v-if="show" outlined max-width="450" class="mx-auto my-2">
-      <v-card-title :class="{'ltr': lang === 'English', 'rtl': lang === 'Hebrew'}">{{card.word}}</v-card-title>
+      <v-card-title style="font-size: 1.5rem" :class="{'ltr': lang === 'English', 'rtl': lang === 'Hebrew'}">{{card.word}}</v-card-title>
       <v-card-subtitle :class="{'ltr': lang === 'English', 'rtl': lang === 'Hebrew'}">{{card.pos}}</v-card-subtitle>
-      <v-card-text class="text-body-1" :class="{'rtl': lang === 'English', 'ltr': lang === 'Hebrew'}">
+      <v-card-text style="font-size: 1.25rem; line-height: 1.6rem" :class="{'rtl': lang === 'English', 'ltr': lang === 'Hebrew'}">
         <div v-for="trans in card.translation" :key="trans">{{trans}}</div>
       </v-card-text>
     </v-card>
