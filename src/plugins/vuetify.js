97638de7e05840232434b89ca260c348b6d7c63d
outofink <moshekrum@gmail.com>
2020-06-28 11:44:52 +0300

    icon orginazation

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-import MorfixIcon from '@/components/MorfixIcon.vue'
+import MorfixIcon from '@/icons/MorfixIcon.vue'
 
 Vue.use(Vuetify);
 
