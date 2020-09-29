180e8b9b37927d70d5c103b2c1ee2a9dd46bb841
outofink <moshekrum@gmail.com>
2020-09-30 00:10:50 +0300

    nuke branch

--- views/js/taptosearch.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('main').addEventListener('click', function(e) {
-  if (e.target.tagName !== 'H3' && e.target.tagName !== 'MAIN') return;
-  const query = document.getElementById('search');
-  query.focus();
-});
--- workbox-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "globDirectory": "public/",
-  "globPatterns": [
-    "**/*.{css,png,js,json,html}"
-  ],
-  "swDest": "public/service-worker.js",
-  "swSrc": "pre-sw.js"
-};
